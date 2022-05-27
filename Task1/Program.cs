4f03a97e8a5c3e1ce49819fa8594ca11cdf2bead
DENIS BLINOV <blinovda1@mail.ru>
2022-05-27 06:55:07 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5->243
+// 2, 4->16
+
+Console.Clear();
+Console.WriteLine("Please, enter two numbers: ");
+
+string s_n_1 = Console.ReadLine();
+int a = int.Parse(s_n_1);
+string s_n_2 = Console.ReadLine();
+int b = int.Parse(s_n_2);
+
+int PowNum(int a, int b)
+{
+    int n_pow = a;
+    for (int i = 1; i < b; i++)
+    {
+        n_pow *= a;
+    }
+    return n_pow;
+}
+Console.WriteLine($"{a}, {b} -> {PowNum(a, b)}");
+
