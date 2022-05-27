9544b216675884c82a94fb6f1a8d8552077a2271
DENIS BLINOV <blinovda1@mail.ru>
2022-05-27 07:13:16 +0300

    add task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Please, enter number: ");
+
+string s_n_1 = Console.ReadLine();
+int num = int.Parse(s_n_1);
+
+int SumNums(int num)
+{
+    int n_sum = 0;
+
+    while (num > 0)
+    {
+        n_sum += num % 10;
+        num /= 10;
+    }
+    return n_sum;
+}
+Console.WriteLine($"{num} -> {SumNums(num)}");
+
+
