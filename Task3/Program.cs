b9acce64b9803d718b6109ba370e6b68add593ae
DENIS BLINOV <blinovda1@mail.ru>
2022-05-27 07:31:54 +0300

    add Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и 
+// выводит их на экран. Оформите заполнение массива и вывод в виде функции.
+// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8
+// 6,1,33,5,6,7,7,1 -> 6,1,33,5,6,7,7,1
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] EightMass()
+{
+    int size = 8;
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+int[] arr_1 = EightMass();
+Print(arr_1);
+int[] arr_2 = EightMass();
+Print(arr_2);
